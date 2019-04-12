ba9c780151b271d5fa9004ca8bd3f04a4a4f761a
dotdidik <didik.galdikaz@gmail.com>
2019-04-12 19:31:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>
-            ISHO ORA
+            ISHO ORA  eeee
           </h1>
         </header>
       </div>
